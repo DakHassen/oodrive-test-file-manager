00e9b61fc0c6b1bc2a738e91374c9b4533b6c807
DakHassen <hassendakhlaoui@hotmail.com>
2020-07-28 22:23:42 +0100

    first maj

--- src/components/ActionComponent.tsx
+++ src/components/ActionComponent.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 // import { useDispatch } from 'react-redux';
 // import allActions from '../actions/allActions';
 import { saveAs } from 'file-saver';
@@ -6,13 +6,67 @@ import Message from './Message';
 import Progress from './Progress';
 import axios from 'axios';
 
+type ItemType = {
+  creation: string,
+  folder: boolean
+  id: string
+  modification: string
+  name: string
+  _links:
+  {
+    children: { href: string, type: string },
+    delete: { href: string, type: string }
+    update: { href: string, type: string }
+    upload: { href: string, type: string }
+  }
+}
+
+type AllItemsType = {
+  items: ItemType[], _links: {
+    '@create': { href: string, type: string },
+    '@upload': { href: string, type: string }
+  }
+
+}
+
 const ActionComponent: any = () => {
   const [file, setFile] = useState('');
   const [filename, setFilename] = useState('Choisissez votre fichier :) ');
-  const [uploadedFile, setUploadedFile] = useState({ name: "", id: "", folder: false });
   const [message, setMessage] = useState('');
   const [uploadPercentage, setUploadPercentage] = useState(0);
+  const [currentParentID, setcurrentParentID] = useState("");
+  const [breadcrumb, setbreadcrumb] = useState(["Home"]);
+  const [allItems, setAllItems] = useState<AllItemsType>(
+    {
+      items: [{
+        creation: "",
+        folder: false,
+        id: "",
+        modification: "",
+        name: "",
+        _links:
+        {
+          children: { href: "", type: "" },
+          delete: { href: "", type: "" },
+          update: { href: "", type: "" },
+          upload: { href: "", type: "" },
+        }
+      }], _links: {
+        '@create': { href: "", type: "" },
+        '@upload': { href: "", type: "" }
+      }
+    }
+  );
 
+
+
+  useEffect(() => {
+
+    axios.get("/api/items").then((res) => {
+      setAllItems(res.data)
+    })
+
+  }, []);
 
   const onChange = (e: any) => {
     setFile(e.target.files[0]);
@@ -25,7 +79,7 @@ const ActionComponent: any = () => {
     formData.append('file', file);
 
     try {
-      const res = await axios.post('/api/items', formData, {
+      const res = await axios.post(`/api/items?parentId=${currentParentID}`, formData, {
         headers: {
           'Content-Type': 'multipart/form-data'
         },
@@ -40,12 +94,16 @@ const ActionComponent: any = () => {
           setTimeout(() => setUploadPercentage(0), 10000);
         }
       });
+      let temp = allItems;
+      res.data.items.forEach((element: ItemType) => {
+        return temp.items.push(element);
+      }
 
-      const { name, id, folder } = res.data.items[0];
+      );
 
-      setUploadedFile({ name, id, folder });
-
+      setAllItems(temp);
       setMessage('File Uploaded');
+
     } catch (err) {
       if (err.response.status === 500) {
         setMessage('There was a problem with the server');
@@ -57,43 +115,45 @@ const ActionComponent: any = () => {
 
   const createFolder = async (e: any) => {
     e.preventDefault();
-
-    try {
-      const res = await axios.post('/api/items', { name: e.target.name.value, folder: true }, {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-      console.log(res.data.items[0])
-      const { name, id, folder } = res.data.items[0];
-      setUploadedFile({ name, id, folder });
-
+    await axios.post(`/api/items?parentId=${currentParentID}`, { name: e.target.name.value, folder: true }).then((res) => {
+  
+      let temp = allItems;
+      temp.items.push(res.data);
+      setAllItems(temp);
       setMessage('Folder Created');
-    } catch (err) {
+    }
+    ).catch(err => {
+      console.log(err.response)
       if (err.response.status === 500) {
         setMessage('There was a problem with the server');
       } else {
         setMessage(err.response.data.desc);
       }
-    }
+    })
   };
-  const downloadFile = async (e: any) => {
-    e.preventDefault();
 
-    try {
-      // const res = await axios.get(`/api/items/${uploadedFile.id}`, {
-      //   responseType: 'arraybuffer'
-      // });
-      saveAs(`/api/items/${uploadedFile.id}`);
-      setMessage('Folder Downlowded');
-    } catch (err) {
+  const accesTofolder = async (e: any, childrenHref: string, name: string, id: string) => {
+    e.preventDefault();
+    let temp = breadcrumb;
+    temp.push(name)
+    setbreadcrumb(temp);
+    setcurrentParentID(id)
+    await axios.get(childrenHref).then((res) => {
+      console.log(res.data)
+      setAllItems(res.data)
+    }
+    ).catch(err => {
       if (err.response.status === 500) {
         setMessage('There was a problem with the server');
       } else {
         setMessage(err.response.data.desc);
       }
-    }
-
+    })
+  };
+  const downloadFile = async (e: any, id: string) => {
+    e.preventDefault();
+    saveAs(`/api/items/${id}`);
+    setMessage('Folder Downlowded');
   }
 
   return (
@@ -141,33 +201,48 @@ const ActionComponent: any = () => {
         />
       </form>
       <div className='column mt-5'>
-        <nav aria-label="breadcrumb">
-          <ol className="breadcrumb">
-            <li className="breadcrumb-item"><a href="#">Home</a></li>
-            {uploadedFile && uploadedFile.id !== "" && uploadedFile.folder ? (<div>
-              <li className="breadcrumb-item"><a href="#">{uploadedFile.name}</a></li>
-            </div>) : <li className="breadcrumb-item"><a href="#"> </a></li>}
+        <div className="card text-center">
+          <div className="card-header">
+            <nav aria-label="breadcrumb">
+              <ol className="breadcrumb">
+                {breadcrumb.map((br, i) => {
+
+                  return <li key={i} className="breadcrumb-item"><a href="#">{br}</a></li>
+                })}
+
+              </ol>
+            </nav>
+          </div>
+          <div className="card-body">
+            <div className="container-fluid content-row">
+              <div className="row">
+                {allItems.items.map((item, i) => {
+                  return <div className="col-sm-4 col-lg-2"> <div key={i} className="card h-100">
+
+                    {item.folder ? <i className="fa fa-folder fa-5x" aria-hidden="true"></i>
+                      : <img className="card-img-top" src={`/api/items/${item.id}`} alt='' />}
+
+                    <div className="card-body">
+                      <h6 className='text-center'>{item.name}</h6>
+                    </div>
+                    <div className="card-footer">
+                      {item.folder ? <button type="button" className="btn btn-info btn-block" onClick={() => accesTofolder(event, item._links.children.href, item.name, item.id)} >Ouvrir</button> : <button type="button" className="btn btn-info btn-block" onClick={() => downloadFile(event, item.id)} >Téléchargez</button>}
+
+                    </div>
+                  </div></div>
+                })}
+              </div>
 
 
-          </ol>
-        </nav>
-      </div>
-      {uploadedFile && uploadedFile.id !== "" ? (
-
-        <div className="card-group">
-          <div className="card">
-            <img style={{ width: '100%' }} src={`/api/items/${uploadedFile.id}`} alt='' />
-
-            <div className="card-footer">
-              <h3 className='text-center'>{uploadedFile.name}</h3>
-
-              <button type="button" className="btn btn-info btn-block" onClick={downloadFile} >Téléchargez</button>
             </div>
           </div>
-        </div>
+          <div className="card-footer text-muted">
+            2 days ago
+  </div></div>
 
-      ) : null
-      }
+
+      </div>
+
     </Fragment>
   );
 }
